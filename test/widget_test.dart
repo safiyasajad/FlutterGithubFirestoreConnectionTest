dca9dfd540dad57990d61e428e88fce7dfb44230
Safiya Sajad <safiyasajad@gmail.com>
2026-05-21 16:38:20 +0530

    configurations
    updates of plugins and importation of data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
 
-import 'package:fluttergithubfirestoreconnectiontest/main.dart';
+import 'package:fluttergithubfirestoreconnectiontest/login.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Login page shows auth controls', (WidgetTester tester) async {
+    await tester.pumpWidget(const GetMaterialApp(home: Login()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Login Page'), findsOneWidget);
+    expect(find.text('Email'), findsWidgets);
+    expect(find.text('Password'), findsWidgets);
+    expect(find.text('Login'), findsOneWidget);
+    expect(find.text('Sign Up'), findsOneWidget);
+    expect(find.text('Forgot Password?'), findsOneWidget);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets('Login page validates empty fields', (WidgetTester tester) async {
+    await tester.pumpWidget(const GetMaterialApp(home: Login()));
+
+    await tester.tap(find.text('Login'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Please enter both email and password'), findsOneWidget);
   });
 }
